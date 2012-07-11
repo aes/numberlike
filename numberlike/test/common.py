c5ae7471ba5e00b52e6b83f758e11e578dcb6455
Anders Eurenius <aes@nerdshack.com>
2012-07-11 16:50:59 +0200

    Creating mixin classes for string round trip and comparison tests

--- numberlike/test/common.py
+++ numberlike/test/common.py
@@ -5,8 +5,63 @@ def raises(exc, fun, *al, **kw):
         e, _ = None, fun(*al, **kw)
     except Exception, e:
         pass
-    return type(e) == exc
+    return isinstance(e, exc) and e
 
 
-def assert_raises(exc, fun, al, kw):
-    assert raises(exc, fun, *al, **kw)
+def assert_raises(exc, fun, al=None, kw=None):
+    assert raises(exc, fun, *(al or []), **(kw or {}))
+
+
+class StringRoundTrip(object):
+    def string_checks(self, cls, seq):
+        def repr_eval_check(c, a):
+            x = cls(arg)
+            assert x == eval(repr(x), {c.__name__: c})
+
+        def str_ctor_check(c, a):
+            x = cls(arg)
+            assert x == c(str(x))
+
+        def str_same_check(c, a):
+            x = cls(arg)
+            assert str(x) == str(c(str(x)))
+
+        for arg in seq:
+            yield lambda c, a: repr(c(a)), cls, arg
+            yield lambda c, a: str(c(a)), cls, arg
+            yield repr_eval_check, cls, arg
+            yield str_ctor_check, cls, arg
+            yield str_same_check, cls, arg
+
+
+class Comparisons(object):
+    def comparison_checks(self, cls, seq):
+        def lt(n, o):
+            assert o < n
+
+        def le(n, o):
+            assert o <= n and n <= n
+
+        def ge(n, o):
+            assert n >= n and n >= o
+
+        def gt(n, o):
+            assert n > o
+
+        o = None
+        for n in seq:
+            n = cls(n)
+            for c in lt, le, ge, gt:
+                yield c, n, o
+            o = n
+
+class BooleanLogic(object):
+    def boolean_checks(self, true, false):
+        def _f(f, t):           assert not f
+        def _t(f, t):           assert t
+        def _or(f, t):          assert t or True
+        def _and(f, t):         assert t and True
+        def _ror(f, t):         assert True or t
+        def _rand(f, t):        assert True and t
+        def _coerce_t(f, t):    assert bool(f)
+        def _coerce_f(f, t):    assert not bool(f)
